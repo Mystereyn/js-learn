6c8808140744745cf49d4f7172d69622d1257124
Mystereyn <teukureynaldi106@gmail.com>
2022-10-20 14:57:29 +0700

    updating number9.js

---
+++ number9.js
@@ -0,0 +1,27 @@
+// A keyword called let was introduced in ES6, a major update to JavaScript, to solve this potential issue with the var keyword. You'll learn about other ES6 features in later challenges.
+
+// If you replace var with let in the code above, it results in an error:
+
+// let camper = "James";
+// let camper = "David";
+
+// The error can be seen in your browser console.
+// So unlike var, when you use let, a variable with the same name can only be declared once.
+// Update the code so it only uses the let keyword.
+
+
+
+
+// example using var and using let keyword for choose one of variable to showing true  
+    // var test1 = "Teuku";
+    // var test1 = "rrrr";
+    // console.log(test1);
+
+    // let test2 = "reynaldi";
+    // let test2 = "p";
+
+    // var catName = "Oliver";
+    // var catSound = "Meow!";
+
+    let catName = "Oliver";
+    let catSound = "Meow!";
