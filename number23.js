424eb72a039ce08053304dea122a74a41f1ac20c
Mystereyn <teukureynaldi106@gmail.com>
2022-10-27 10:54:33 +0700

    Continuing learning augmented assignment (/=)

---
+++ number23.js
@@ -0,0 +1,17 @@
+let a = 48;
+let b = 108;
+let c = 33;
+// a should equal 4.
+// Waiting:b should equal 27.
+// Waiting:c should equal 3.
+// Waiting:You should use the /= operator for each variable.
+// Waiting:You should not modify the code above the specified comment.
+
+// Only change code below this line
+// a = a / 12;
+// b = b / 4;
+// c = c / 11;
+
+a /= 12;
+b /= 4;
+c /= 11;
