af6ef05126cf811bc8b2c1e932034ac1373f6814
Mystereyn <teukureynaldi106@gmail.com>
2022-11-09 23:38:33 +0700

    number30.js

---
+++ number30.js
@@ -0,0 +1,13 @@
+// Just as we can build a string over multiple lines out of string literals, we can also append variables to a string using the plus equals (+=) operator.
+
+//  append = menambahkan
+// Example:
+
+// const anAdjective = "awesome!";
+// let ourStr = "freeCodeCamp is ";
+// ourStr += anAdjective;
+
+const someAdjective = "fun to me";
+let myStr = "Learning to code is ";
+
+myStr += someAdjective;
