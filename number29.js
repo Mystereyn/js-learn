c31af3a90251fc60d4e81f64e2b28d2eeff95899
Mystereyn <teukureynaldi106@gmail.com>
2022-11-09 23:20:10 +0700

    commit number29.js

---
+++ number29.js
@@ -0,0 +1,9 @@
+// Constructing String dengan variables
+
+// contoh 
+
+const ourName = "FreeCodeCamp";
+const ourStr = "Hello, our name is " + ourName + ", how are you";
+
+const myName = "Teuku Reynaldi Putra";
+const myStr = "My name is " + myName + "and I am well";
