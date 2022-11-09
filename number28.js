c308670b53d8a4527d0ebc6423cda9077bd51cf7
Mystereyn <teukureynaldi106@gmail.com>
2022-11-09 22:58:38 +0700

    learning combine two values string with using operator of +=

---
+++ number28.js
@@ -0,0 +1,20 @@
+// menggunakan operator += bisa diguanakn 
+// untuk menyambungkan nilai variable
+//  string pertama ke beberapa nilai string
+//  selanjutnya
+
+// sebagai contoh
+let ourStr = "I come first. "
+ourStr += "I come Second.";
+// output || ourStr = "I come first . I come second"
+
+
+
+// myStr should have a single space character between the two strings.
+// Failed:myStr should have a value of the string This is the first sentence. This is the second sentence.
+// Failed:You should use the += operator to build myStr.
+
+// const myStr_1 = "This is the first sentence.";
+
+let myStr = "This is the first sentence. ";
+    myStr += "This is the second sentence";
