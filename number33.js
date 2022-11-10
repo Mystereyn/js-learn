9fd2f69c40308356258d9e9949fd229fd65fecee
Mystereyn <teukureynaldi106@gmail.com>
2022-11-11 00:18:19 +0700

    learning String Immutabily

---
+++ number33.js
@@ -0,0 +1,25 @@
+// #Materi String Immutability
+
+
+
+// Dalam JavaScript, nilai String tidak 
+// dapat diubah, yang artinya tidak dapat 
+// diubah setelah dibuat.
+
+// contoh
+
+// let myStr = "Bob";
+
+// myStr[0] = "J"; // tidak bisa karena, nilainya didepan sudah 
+         // B kapital()
+
+// bisa di ganti dengan memasukan nilai baru
+
+
+// let myStr = "Bob";
+//     myStr = "Teuku";
+
+    // Setup
+let myStr = "Jello World";
+    myStr= "Hello World";
+console.log(myStr);
