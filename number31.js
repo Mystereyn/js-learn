992d65bb6889de17366bcaeb9655be7cf4a296e1
Mystereyn <teukureynaldi106@gmail.com>
2022-11-10 23:32:30 +0700

    Belajar menghitung panjang nilai String di JS

---
+++ number31.js
@@ -0,0 +1,16 @@
+// Materi menentukan panjang nilai String
+
+// console.log("Alan Peter" .length);
+
+// !!! DO not edit codes below
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// !!! Yang salah
+// Only change code below this line
+// lastNameLength = lastName;
+// console.log(lastName.length);
+
+lastNameLength = lastName.length;
+console.log(lastNameLength);
