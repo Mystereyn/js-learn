a2565bf308bd57d15ecf3696aed30752a625891d
Mystereyn <teukureynaldi106@gmail.com>
2022-11-10 23:43:59 +0700

    learning bracket notation, search first letter of value

---
+++ number32.js
@@ -0,0 +1,19 @@
+// materi bracket notation
+
+
+//menentukan huruf pertama dari kata menggunakan index 0;
+// contoh : 
+
+
+// const firstName = "Charles";
+// const firstLetter = firstName[0];
+
+
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+console.log(firstLetterOfLastName);
