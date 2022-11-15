1586b3e461e050a24baca1d12d7b87f565327952
Mystereyn <teukureynaldi106@gmail.com>
2022-11-15 23:07:20 +0700

    Learning popping method

---
+++ number42.js
@@ -0,0 +1,28 @@
+// Materi Pop value array;
+// Pop = mengeluarkan nilai akhir dri array
+
+// const threeArr = [1, 4, 6];
+// const oneDown = threeArr.pop();
+// console.log(oneDown);
+// console.log(threeArr);
+
+
+// !!!! Yang Salah
+
+// Setup
+
+// const myArray = [["John", 23], ["cat", 2]];
+// myArray.pop()
+
+// const myArray = [["John" , 23] , ["cat" , 2]];
+// const removedFromArray = myArray.pop();
+// console.log(removedFromArray);
+// console.log(myArray);
+
+// !!! Yang Benar
+const myArray = [["John" , 23] , ["cat" , 2]];
+
+const removedFromMyArray = myArray.pop();
+console.log(removedFromMyArray);
+console.log(myArray);
+
