a1d2dfbf432426c9084faba3e30ff4fe68055036
Mystereyn <teukureynaldi106@gmail.com>
2022-11-15 13:07:33 +0700

    Learning multidimensional array

---
+++ number40.js
@@ -0,0 +1,32 @@
+const myArray = [ [1,2,3],[4,5,6],[7,8,9],[ [10,11,12],13, 14] ];
+                // idx 0 , idx 1 , idx 2 ,     idx 3, 13? 14?
+
+const myData = myArray[2][1];
+// cara bacanyanya : posisi index array kedua. lalu cari nilai yang indexnya ke 1. contoh 
+// [index 0 = 7, index 1 = 8, index 2 = 9]
+
+
+console.log(myData)
+
+
+
+// In this example, subarray has the value [[10, 11, 12], 13, 14], nestedSubarray has the value [10, 11, 12], and element has the value 11 .
+
+// const subArray0 = arrays[0];
+// const subArray1 = arrays[0];
+// const subArray2 = arrays[1];
+// const subArray3 = arrays[2];
+// const subArray4 = arrays[3];
+
+
+// const nestedSubArray = arrays[3][0];
+// console.log(nestedSubArray);
+
+// const element = arrays[3][0][1];
+// console.log(element);
+
+// console.log(`ini SubArray0 ${subArray0}`);
+// console.log(`ini SubArray1 ${subArray1}`);
+// console.log(`ini SubArray2 ${subArray2}`);
+// console.log(`ini SubArray3 ${subArray3}`);
+// console.log(`ini SubArray4 ${subArray4}`);
