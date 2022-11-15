ef57171ebb83d69ba2ffa1dc2ea6bfebcc4346cf
Mystereyn <teukureynaldi106@gmail.com>
2022-11-15 13:40:41 +0700

    learning appending a value to array

---
+++ number41.js
@@ -0,0 +1,24 @@
+// Materi Append value array
+
+
+//  Append = Tambahkan
+
+// An easy way to append data to the end of an array is via the push() function.
+
+// .push() takes one or more parameters and "pushes" them onto the end of the array.
+
+const arr1 = [1, 2, 3];
+arr1.push(4);
+console.log(arr1);
+
+const arr2 = ["Stimpson", "J", "cat"]; 
+arr2.push(["happy", "joy"]);
+console.log(arr2);
+
+// Setup
+const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+myArray.push(["dog" , 3])
+
+console.log(myArray);
