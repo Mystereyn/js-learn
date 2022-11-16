8eb97f4e2e5104c6d6d399577db796b8c852c431
Mystereyn <teukureynaldi106@gmail.com>
2022-11-16 11:07:03 +0700

    giving 5 elements in 2 dimensional arrays

---
+++ number45.js
@@ -0,0 +1,10 @@
+const myList = [
+    ["Apple" , 1],
+    ["PineApple" , 2],
+    ["WaterMelon" , 3],
+    ["Pear" , 4],
+    ["Orange" , 5]
+];
+
+console.log(myList.length);
+console.table(myList);
