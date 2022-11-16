1e41ff8dc61f6c290616b1f9d430a0b94d99ad08
Mystereyn <teukureynaldi106@gmail.com>
2022-11-16 22:40:28 +0700

    Learning local variable in Function

--- number49.js
+++ number49.js
@@ -31,3 +31,4 @@ function fun2() {
     }
     console.log(output);
 };
+
---
+++ number50.js
@@ -0,0 +1,20 @@
+// Materi Local Scope And Functions
+
+
+// Contoh
+function myTest() {
+    const loc = "foo";
+    console.log(loc);
+};  
+myTest();
+
+console.log(loc); // ini error, karena 
+// variablenya ada di dalam function
+
+function myLocalScope() {
+    const myVar = 'bar';
+    console.log('inside myLocalScope' , myVar);
+};
+myLocalScope();
+
+console.log('outside myLocalScope', myVar);
