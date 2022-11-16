67525804a5ffbc2c1ea32b74489374205a9d7e27
Mystereyn <teukureynaldi106@gmail.com>
2022-11-16 11:44:30 +0700

    learning function 2 parameters

---
+++ number47.js
@@ -0,0 +1,14 @@
+// Materi Function Parameter
+
+// Contoh 
+
+function testFun(param1, param2) {
+    console.log(param1, param2);
+}
+testFun();
+
+function functionWithArgs(a,b) {
+    console.log(a+b);
+};
+functionWithArgs(1,2);
+
