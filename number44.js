cf292ddc196b35c7c5d49de5ac032103841d5119
Mystereyn <teukureynaldi106@gmail.com>
2022-11-16 10:00:44 +0700

    Learning unshift() element array after shift()

---
+++ number44.js
@@ -0,0 +1,21 @@
+// Materi UnShift Array
+
+// unshist() = function yang digunakan untuk 
+// mengisi element kembali setelah shift() function
+
+// Contoh 
+    const ourArray = ["Stimpson" , "J" , "cat"];
+    ourArray.shift();
+    // console.log(`Ini shift value dari ourArray = ${ourArray}`);
+    ourArray.unshift("Happy");
+    console.log(`Ini Shift value dari setelah unShift =  ${ourArray}`);
+
+
+    // Add ["Paul", 35] to the beginning of the myArray variable using unshift().
+    // Setup
+const myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+myArray.unshift(["Paul" , 35])
+console.log(myArray);
+
+// Only change code below this line
