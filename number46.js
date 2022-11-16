1152bc36538a382ebca1ceca238493632cabe4f0
Mystereyn <teukureynaldi106@gmail.com>
2022-11-16 11:12:30 +0700

    Learning function

---
+++ number46.js
@@ -0,0 +1,13 @@
+// Materi Function JS
+// Invoke func J atau call function
+// Contoh
+
+function functionName() {
+    console.log("Hello World");
+}; 
+functionName();
+
+function reusableFunction() {
+    console.log("Hi World");
+};
+reusableFunction();
