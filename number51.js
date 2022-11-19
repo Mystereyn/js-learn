afbb1f7354188a46309525773978f933e79b84ce
Mystereyn <teukureynaldi106@gmail.com>
2022-11-19 10:05:03 +0700

    [UPDATED] Learning Global vs Local Scope in Func

--- number51.js
+++ number51.js
@@ -2,11 +2,26 @@
 
 // Contoh 
 
-
+// Global Variable
 const someVar = "Hat";
 
 function myFun() {
+
+  // Local Variable tpi nama variable sama
   const someVar = "Head";
+  // return untuk kembalikan value variable someVar di local scope
+  // dan juga mengubah value global menjadi local value
   return someVar;
 };
+// mencetak funsi local scope variable 
 console.log(myFun(someVar));
+
+
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  const outerWear = "Sweater"
+  return outerWear;
+};
+
+console.log(myOutfit(outerWear));
