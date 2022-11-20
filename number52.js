df9bae474354594fca032b814abe97857cd76195
Mystereyn <teukureynaldi106@gmail.com>
2022-11-20 10:39:52 +0700

    Learning Assignment with Return Value from func

---
+++ number52.js
@@ -0,0 +1,34 @@
+// assignment dengan return value
+
+// function sum
+// ourSum = sum(5,12);
+
+
+// Call the processArg function with an argument of 7 and assign its return value to the variable processed.
+
+
+// Waiting:processed should have a value of 2
+// Waiting:You should assign processArg to processed
+
+
+// // Setup
+// let processed = 7;
+// // let num;
+// function processArg(num) {
+//     // var num = 7
+//     return (num + 3) / 5;
+//     // console.log(num);
+// };
+// console.log(processArg(num));
+// // console.log(num);
+
+
+let processed = 0;
+
+function processArg(num) {
+    return (num + 3) / 5;
+};
+
+// Only change code below this line
+processed = processArg(7);
+console.log(processed)
