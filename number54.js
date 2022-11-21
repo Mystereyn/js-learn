e25b8bcb42e5d7758fad401b4ce1f12fc48e1e4e
Mystereyn <teukureynaldi106@gmail.com>
2022-11-21 08:38:55 +0700

    Stand InLine FreeCodeCamp

---
+++ number54.js
@@ -0,0 +1,26 @@
+// Materi IF
+
+// If adalah statement yang bervalue true atau false
+
+// If Selalu digunakan ketika ingin melakukan sebuah filterisasi.
+
+//* Kalau nilainya itu berkondisikan true, maka program yang di dalam kurung kurawal akan di eksekusi
+//* dan kalau nilainya itu berkondisikan false, maka program yang diluar kurung kurawal akan di eksekusi
+
+// function trueOrFalse() {
+//     if (trueOrFalse(true)) {
+//         return "Yes, that was true";
+//     } else {
+//         return "No, that was false";
+//     }
+// };
+
+
+function trueOrFalse() {
+    if(trueOrFalse) {
+        return "Yes that was true";
+    }
+    return "No that was false";
+}; 
+
+console.log(trueOrFalse());
