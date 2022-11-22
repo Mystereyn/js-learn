adcee35f13f70ea48552106a90b5b99d7335359c
Mystereyn <teukureynaldi106@gmail.com>
2022-11-22 21:43:38 +0700

    Comparison condition value with if statement but the value in func callback

---
+++ number55.js
@@ -0,0 +1,9 @@
+function testEqual(val) {
+    if(val == 10 ) {
+        return "Not Equal";
+    }
+    return "Equal";
+}
+// https://github.com/Mystereyn/Free-Code-Camp-Exercise
+
+console.log(testEqual("12"));
