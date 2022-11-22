9c41230ef5e7785cb437f702738ba293d1b64938
Mystereyn <teukureynaldi106@gmail.com>
2022-11-22 21:56:50 +0700

    Test git error bug : failed to push to remote, after change commit on remote

--- number55.js
+++ number55.js
@@ -4,6 +4,9 @@ function testEqual(val) {
     }
     return "Equal";
 }
+<<<<<<< HEAD
 // https://github.com/Mystereyn/Free-Code-Camp-Exercise
+=======
+>>>>>>> 8651a0d4d4c43d0c1c9cf879ed4bd594aa3228ba
 
 console.log(testEqual("12"));
