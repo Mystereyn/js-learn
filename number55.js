286da7dc2c6bd62301d7acb924b9e5022b8e5813
Mystereyn <teukureynaldi106@gmail.com>
2022-11-27 00:58:52 +0700

    Compared a condition using operator == or  ===

--- number55.js
+++ number55.js
@@ -1,3 +1,8 @@
+//materi compare with equality operand
+// operator equality "==";
+// digunakan buat perbandingan valuenya saja
+
+
 function testEqual(val) {
     if(val == 10 ) {
         return "Not Equal";
@@ -5,3 +10,21 @@ function testEqual(val) {
     return "Equal";
 }
 // https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/comparison-with-the-equality-operator
+
+
+
+
+// strict equliaty
+// Operator yang menentukan true/false
+//perbandingan untuk tipe data, variable dan valuenya
+
+
+function testStrict(val) {
+    if(val === 7) {
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+console.log(testStrict(7));
+
---
+++ number56.js
@@ -0,0 +1,14 @@
+// strict equliaty
+// Operator yang menentukan true/false
+
+//perbandingan untuk tipe data,
+
+
+function testStrict(val) {
+    if(val === 7) {
+        return "Equal";
+    }
+    return "Not Equal";
+}
+
+console.log(testStrict(7));
