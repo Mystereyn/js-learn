daa31ea2bca03a543058012f15d5486bad58eafc
Mystereyn <teukureynaldi106@gmail.com>
2022-11-29 01:57:35 +0700

    Learning !== Operator StrictNot  Equality

--- number55.js
+++ number55.js
@@ -3,12 +3,12 @@
 // digunakan buat perbandingan valuenya saja
 
 
-function testEqual(val) {
-    if(val == 10 ) {
-        return "Not Equal";
-    }
-    return "Equal";
-}
+// function testEqual(val) {
+//     if(val == 10 ) {
+//         return "Not Equal";
+//     }
+//     return "Equal";
+// }
 // https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/comparison-with-the-equality-operator
 
 
@@ -19,36 +19,49 @@ function testEqual(val) {
 //perbandingan untuk tipe data, variable dan valuenya
 
 
-function testStrict(val) {
-    if(val === 7) {
-        return "Equal";
-    }
-    return "Not Equal";
-}
+// function testStrict(val) {
+//     if(val === 7) {
+//         return "Equal";
+//     }
+//     return "Not Equal";
+// }
 
-console.log(testStrict(7));
+// console.log(testStrict(7));
 
 
 // Method typeOf
 
-function compareEquality(a,b) {
-    if(typeof(a === b)) {
-        return "Not Equal";
-    }
-    return "Equal";
-}
-console.log(compareEquality("10" , 10));
+// function compareEquality(a,b) {
+//     if(typeof(a === b)) {
+//         return "Not Equal";
+//     }
+//     return "Equal";
+// }
+// console.log(compareEquality("10" , 10));
 
 
 /** 
  *  != digunakan buat perbandingan tidak cocoknya 2 nilai 
  * 
  * */
-function testNotEqual(val) {
-    if(val != 99) {
+// function testNotEqual(val) {
+//     if(val != 99) {
+//         return "Not Equal";
+//     }
+//     return "Equal";
+// };
+
+// console.log(testNotEqual("99"));
+
+// !== digunakan jika ingin perbandingan tipe data value dari variable
+
+function testStrictNotEqual (val) {
+    if(val !== 17) {
         return "Not Equal";
     }
     return "Equal";
 };
-
-console.log(testNotEqual("99"));
+console.log(testStrictNotEqual(17));
+console.log(testStrictNotEqual("17"));
+console.log(testStrictNotEqual(12));
+console.log(testStrictNotEqual("Bob"));
