c60ec087cb18706362ee9996284d4fc1a1141962
Mystereyn <teukureynaldi106@gmail.com>
2022-12-05 22:20:32 +0700

    What is ... in js? (Chaining if, else if, else)

--- ifELse.js
+++ ifELse.js
@@ -28,3 +28,26 @@ function testElseIf(val) {
 console.log(testElseIf(0));
 console.log(testElseIf(5));
 console.log(testElseIf(12));
+
+// Chaining if else else if
+function testSize(value) {
+    if(value < 5) {
+        return "Tiny";
+    } else if (value < 10) {
+        return "Small";
+    } else if (value < 15) {
+        return "Medium";
+    } else if (value < 20) {
+        return "Large"
+    } else {
+        return "Huge";
+    }
+};
+
+console.log(testSize(0));
+console.log(testSize(4));
+console.log(testSize(5));
+console.log(testSize(8));
+console.log(testSize(10));
+console.log(testSize(15));
+console.log(testSize(25));
