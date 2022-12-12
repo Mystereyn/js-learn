5b24445dd90a1ca391319558be8388ec3ab52934
Mystereyn <teukureynaldi106@gmail.com>
2022-12-12 10:21:22 +0700

    [Update] , masih banyak yang salah

--- switchCase.js
+++ switchCase.js
@@ -88,19 +88,22 @@ function sequantialSizes(val) {
     let answer = "";
     
     switch(val) {
-        case 1 : return "Low";
-        case 2 : return "Low";
-        case 3 : return "Low";
+        case 1 : answer = "Low";
+        case 2 : answer = "Low";
+        case 3 : answer = "Low";
+                 break;
 
-        case 4 : return "Mid";
-        case 5 : return "Mid";
+        case 4 : answer = "Mid";
+        case 5 : answer = "Mid";
+        case 6 : answer = "Mid";
+                 break;
 
-        case 6 : return "Mid";
-        case 7 : return "High";
-        case 8 : return "High";
-        case 9 : return "High";
-
+        case 7 : answer = "High";
+        case 8 : answer = "High";
+        case 9 : answer = "High";
+                 break;
     };
+    return answer;
 };
 
 console.log(sequantialSizes(1));
@@ -114,5 +117,3 @@ console.log(sequantialSizes(6));
 console.log(sequantialSizes(7));
 console.log(sequantialSizes(8));
 console.log(sequantialSizes(9));
-
-
