fc88875187e2c1b720af3e11548c136471e239df
Mystereyn <teukureynaldi106@gmail.com>
2022-12-13 20:23:26 +0700

    Math JS, belum ngerti di Math Round

--- Math.js
+++ Math.js
@@ -2,5 +2,16 @@
 
 // Cara Baca : 
 // Math.pow(x , y)= X^y; 
+
 // 
+function mathPow() ;
 console.log(Math.pow(7,3));
+
+// Math.Round = Method balikin nilai integer paling deket
+ function mathRound(a = 0.5, b = 0.9) {
+    return console.log(Math.round(a,b));
+
+};
+mathRound();
+
+
