e669c8832130572dc85ea856bc07bf1e52b517db
Mystereyn <teukureynaldi106@gmail.com>
2022-12-14 09:26:27 +0700

    [Update]

--- Math.js
+++ Math.js
@@ -4,14 +4,32 @@
 // Math.pow(x , y)= X^y; 
 
 // 
-function mathPow() ;
-console.log(Math.pow(7,3));
+function mathPow() {
+    console.log(Math.pow(7,3));
+}; 
+mathPow();
 
 // Math.Round = Method balikin nilai integer paling deket
  function mathRound(a = 0.5, b = 0.9) {
     return console.log(Math.round(a,b));
-
 };
 mathRound();
+
+function abTest(a,b) {
+    // if(a <= 0 || b <= 0) {
+    //     console.log(undefined);
+    // }
+
+    if(a < 0 || b < 0) {
+        return undefined; //primitive types = tidak membuthkan tanda string buat eksekusi
+    };
+    // else if (a && b == 0){
+    //     return 0;
+    // }
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2)); //gatau baris code ini gunanya apa?
+};
+
+console.log(abTest(2,2));
+console.log(abTest(0,0));
 
 
