f00608f9b947411af6e199f8957ee3699e39dfdc
Mystereyn <teukureynaldi106@gmail.com>
2022-12-15 11:46:53 +0700

    [Updated] , ga ngerti isi codenya

--- Card.js
+++ Card.js
@@ -1,9 +1,35 @@
 let count = 0;
 
 function cc(card) {
-    if(card > 2) {
-        return count+=1 + "Bet";
-    } else {
-        return count-=1 + "Hold";
-    }
+
+    switch(card) {
+        // cartu bertambah tiap 1 tingkat
+        case 2 :
+        case 3 :
+        case 4 :
+        case 5 :
+        case 6 :
+            count++;
+            break;
+        // kartu beerkurang 1 tingkat tiap flip  
+        case 10 :
+        case "J":
+        case "Q":
+        case "K":
+        case "A":
+            count--;
+            break;
+    };
+
+    let holdbet = "Hold";
+
+    if(count > 0) {
+        holdbet = "Bet";
+    };
+    return count + " " + holdbet;
 };
+console.log(cc(2)); 
+console.log(cc(3)); 
+console.log(cc(7)); 
+console.log(cc('K')); 
+console.log(cc('A'));
