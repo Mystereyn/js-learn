a480a2cce5789751e74b341c3bd0138883141da1
Mystereyn <teukureynaldi106@gmail.com>
2022-12-16 09:44:56 +0700

    Commit

---
+++ BuildObject.js
@@ -0,0 +1,22 @@
+const myDog = {
+    name : "Bull",
+    legs : 4,
+    tails : 1,
+    friends : ["Cat" , "Ball" , "Bone" , "Milk"] 
+};
+
+console.log(myDog);
+console.log("\n");
+
+//Access object properties with Dot (.);
+const testObj = {
+    "hat" : "ballcap",
+    "shirt" : "jersey",
+    "shoes" : "cleats"
+};
+
+const hatValue = testObj.hat;
+const shirtValue = testObj.shirt;
+const shoesValue = testObj.shoes;
+
+console.log(hatValue,shirtValue,shoesValue);
