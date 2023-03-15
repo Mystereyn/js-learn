792c7b722f11a02050a694a9ed8912ff2b7292c5
Mystereyn <teukureynaldi106@gmail.com>
2023-03-15 20:31:22 +0700

    push

--- OOP/TipeDataMap.js
+++ OOP/TipeDataMap.js
@@ -4,8 +4,30 @@
 
 const food = ["Nasi Goreng" , "Kwetiau" , "Pecel Lele"];
 
+//! Convert value string ke Array
 let map = Array.prototype.map
 
 const newFood = map.call(food[0] , parseLetter => {return `${parseLetter}` })
 
 console.log(newFood)
+
+
+function useForEach() {
+    // let newDatas;
+    // const datas = {
+    //     name : "Mystereyn",
+    //     Age : 21,
+    //     Address : "JL.Raya Bogor, Km.25"
+    // }
+    let multipleNames = ["Teuku", "Reynaldi" , "Putra", "Irwansyah"];
+
+    // for(const values in multipleNames) {
+    //     console.log(values)
+    // }
+    let tampung;
+    
+    multipleNames.forEach(tampung => {
+        console.log(tampung)
+    });
+}
+useForEach()
