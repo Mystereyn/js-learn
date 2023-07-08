39654569013c47997f0cfbf3798c891696a0e65f
Teuku Reynaldi <teukureynaldi106@gmail.com>
2023-07-09 00:31:15 +0700

    added

--- pzn/closure.js
+++ pzn/closure.js
@@ -1,10 +1,20 @@
-function createAdder(value){
-    const ownerShip = "Teuku";
+// closure = kombinasi function dan bundel refrensei ke data sekitarnya
+// contoh bundel refrensinya itu variable const ownerShip;
+
+function createAdder(value){ // function yang menampung nested func, 
+                             //value disini untuk mencetak nilai integer
+
+    const ownerShip = "Teuku"; // global scope
+    
+    // function  
     function add(param){
-        console.log(ownerShip)
+        console.log(ownerShip) //global scope yang menjadi local scope
         return value+param;
     };
-    return add;
+    return add; // alasan untuk return function ini adalah tergantung
+                // jika dia ada merefrensikan global scope, 
+                // maka return di global scope.
+
 }
 
 const addTwo = createAdder(2);
