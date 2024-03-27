e4712c7a41d8e1c32d247d690b2e5a5915bdad1f
teukureynld <teukureynaldi106@gmail.com>
2024-03-28 00:48:35 +0700

    add

---
+++ pzn/materi_dasar/if_in_switch_case.js
@@ -0,0 +1,44 @@
+// let day = "Wednesday";
+// switch(day){
+//     case "Day 1":
+//         if(day==="Monday"){
+//             console.log("Not true")
+//         }
+//     break;
+
+//     case "Day 2":
+//         if(day==="Tuestday"){
+//             console.log("Not True Again");
+//         }
+//     break;
+
+//     case "Day 3":
+//         if(day==="Wednesday"){
+//             console.log(`That's true, ${day} is the third day`);
+//         }
+//     break;
+// }
+let day = "Wednesday";
+
+switch (day) {
+    case "Monday":
+        if (day === "Monday") {
+            console.log("Not true");
+        }
+        break;
+
+    case "Tuesday":
+        if (day === "Tuesday") {
+            console.log("Not true again");
+        }
+        break;
+
+    case "Wednesday":
+        if (day === "Wednesday") {
+            console.log(`That's true, ${day} is the third day`);
+        }
+        break;
+
+    default:
+        console.log("Hello TeukuReynaldi");
+}
