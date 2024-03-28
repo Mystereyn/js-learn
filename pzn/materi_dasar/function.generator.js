ddb2338668a8bc1dea876aba7fae2d6fd9690189
teukureynld <teukureynaldi106@gmail.com>
2024-03-28 23:26:47 +0700

    add

--- pzn/materi_dasar/function.generator.js
+++ pzn/materi_dasar/function.generator.js
@@ -28,9 +28,11 @@ for(const datas of profile){
 // so that we conclude is normally function that the only which could be as the generator function
 
 // use yield if the function is generator
+// this is lazy
 function* generateEvenNumber(value){
     for(let i = 0; i <= value; i++){
         if(i%2 === 1){
+            console.log(`Yield ${i}`);
             yield i;
         }
     }
@@ -38,5 +40,23 @@ function* generateEvenNumber(value){
 
 const evenNumber = generateEvenNumber(100);
 for(const iterationNumber of evenNumber){
+    console.log(iterationNumber);
+}
+
+
+// this is Eager Function
+function* generateOddNumber(value){
+    const result = [];
+    for(let i = 0; i <= value; i++){
+        if(i%2 === 0){
+            console.log(`Yield ${i}`);
+            result.push(i);
+        }
+    }
+    return result;
+}
+
+const oddNumber = generateOddNumber(100);
+for(const iterationNumber of oddNumber){
     console.log(iterationNumber);
 }
