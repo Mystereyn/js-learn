216c4ac093405fb17c3a32b78404b34082a20b4d
teukureynld <teukureynaldi106@gmail.com>
2024-03-28 20:13:45 +0700

    add

--- pzn/materi_dasar/function.generator.js
+++ pzn/materi_dasar/function.generator.js
@@ -27,3 +27,16 @@ for(const datas of profile){
 
 // so that we conclude is normally function that the only which could be as the generator function
 
+// use yield if the function is generator
+function* generateEvenNumber(value){
+    for(let i = 0; i <= value; i++){
+        if(i%2 === 1){
+            yield i;
+        }
+    }
+}
+
+const evenNumber = generateEvenNumber(100);
+for(const iterationNumber of evenNumber){
+    console.log(iterationNumber);
+}
