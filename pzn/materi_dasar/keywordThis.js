655eebd653ecb40ecb4e5ad63e5ab6485af8daf9
Teuku Reynaldi <teukureynaldi106@gmail.com>
2024-04-15 13:34:27 +0700

    push

--- pzn/materi_dasar/keywordThis.js
+++ pzn/materi_dasar/keywordThis.js
@@ -9,3 +9,21 @@ const personalInfo = {
 }
 console.log(personalInfo.txtGreetings(`Hello ${personalInfo.name}`));
 
+const person = {
+    name : "Teuku Reynaldi",
+    txtHello : function(name2){
+        console.log(`Hello ${name2} this is my friend name is ${this.name}` )
+    }
+}
+person.txtHello("Putra")
+
+// try with return nothing
+// the result is undefined because the function not returning a value.
+const anotherPerson = {
+    name2 : "Teuku Reynaldi Putra",
+    txtGreetings : ()=>{
+        return;
+    }
+}
+
+console.log(anotherPerson.txtGreetings(`Hello ${this.name2}`))
