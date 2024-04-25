589dfcd625bde6d796f4be28e1f2daf689fa553e
Teuku Reynaldi <teukureynaldi106@gmail.com>
2024-04-25 09:20:10 +0700

    commit

--- pzn/materi_dasar/destructing.js
+++ pzn/materi_dasar/destructing.js
@@ -1,10 +1,9 @@
 /**
  * Destructing -> Fitur yang digunakan untuk bongkar value di array atau object ke dalam variable2
- * Fitur ini tanpa harus ambil data/nilai dari array atau object satu satu 
- * 
- * 
+ * Fitur ini tanpa harus ambil data/nilai dari array atau object satu satu
+ *
+ *
  */
-
 
 let a, b, rest;
 [a, b] = [10, 20];
@@ -17,12 +16,49 @@ console.log(b);
 
 console.log(rest);
 
-
-const arrays1 = [1,2,3,4,5,6,7,8,9,10];
-const arrays2 = [11,12,13,14,15];
+const arrays1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const arrays2 = [11, 12, 13, 14, 15];
 
 const concating = [...arrays1, ...arrays2];
 
 const datas = concating;
 console.log(datas);
 
+(() => {
+  let arrays = [1, 2, 3, 4, 5, 6, 8, 10];
+  let [y] = [arrays];
+  console.log(y);
+})();
+
+
+
+
+
+const obj = {
+  name : "Teuku",
+  age : 22
+}
+
+// const getName = obj.name; console.log(getName)
+const {name, age} = obj;
+console.log(`Hai, my name is ${name} and my age is ${age}`)
+
+// destrucuring dengan function paramter
+
+function DestructorDisplayParameter({firstName, middleName,Address}){
+  // Jika tidak menambahkan operator destructor atau object => {}, maka hanya satu data aja yang muncul
+  // jika pake {} , maka semua data muncul
+  return {firstName,middleName,Address};
+}
+
+function DataPersonal(){
+
+  // let extract = DestructorDisplayParameter(()=>{
+    let PersonalInfo = {
+      firstName :"Teuku",
+      middleName: "Reynaldi",
+      Address: "JL. Raya Bogor"
+    }
+    return DestructorDisplayParameter(PersonalInfo);
+}
+console.log(DataPersonal());
