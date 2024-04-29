738cb0a211d62358c09bc5ab6e3c0cd73b399292
Teuku Reynaldi <teukureynaldi106@gmail.com>
2024-04-29 19:04:26 +0700

    push

---
+++ pzn/OOP/Constructor.Function.js
@@ -0,0 +1,25 @@
+// const budi = {
+//     namaDepan : "T",
+//     namaBelakang : "B"
+// }
+
+// const rey = {
+//     namaDepan : "P",
+//     namaBelakang : "V"
+// }
+
+
+// constructor function, memakai UpperCase untuk nama function karena best practisenya memakai ini
+void function Person(){  }
+
+const {
+    
+    firstName = "Teuku", 
+    middleName = "Reynaldi", 
+    lastName = "Putra"
+
+} = new Person();
+
+const Arr = [firstName, middleName, lastName];
+console.log(Arr);
+
