999ecf9e2c11714da9dba4cec5def800d83d8c4c
Teuku Reynaldi <teukureynaldi106@gmail.com>
2024-05-01 10:55:52 +0700

    del folder oop

--- OOP/Student.js
+++
@@ -1,6 +0,0 @@
-class Student {
-    constructor(){
-        this.studentA = this.studentA;
-        this.studentB = this.studentB;
-    }
-}
--- pzn/OOP/Constructor.Function.js
+++ pzn/OOP/Constructor.Function.js
@@ -10,7 +10,9 @@
 
 
 // constructor function, memakai UpperCase untuk nama function karena best practisenya memakai ini
-function Person(){  }  
+function Person(){
+
+}  
 
 const {
     
@@ -24,3 +26,14 @@ const Arr = [firstName, middleName, lastName];
 console.log(Arr);
 
 
+
+function ParameterConstructor(first,last){
+    this.first = first;
+    this.Last = last;
+    this.txtGreetings = function(){
+        return `Hello My firstname is ${this.first} and my last name is ${this.Last}`
+    }
+}
+
+let me = new ParameterConstructor("Teuku Reynaldi", "Putra");
+console.log(me.txtGreetings())
