8b7bff08cbfb922f433e78e551f46d5d6503e774
Teuku Reynaldi <teukureynaldi106@gmail.com>
2024-07-18 18:14:45 +0700

    commit

---
+++ pzn/StandardLibrary/Day.js
@@ -0,0 +1,15 @@
+const date= new Date();
+
+let hour = date.getHours();
+let minutes = date.getMinutes();
+let seconds = date.getSeconds();
+const day = date.toDateString()
+
+function intervalClock(){
+    setInterval(()=>{
+        
+        const logDate = `Hari: ${day}, jam: ${hour}:${minutes}:${seconds}`;
+        console.log(logDate);
+    }, 1000)
+}
+intervalClock();
