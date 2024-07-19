4ab91e36040bd562d9f8b315e4a2fa50ab624577
Teuku Reynaldi <teukureynaldi106@gmail.com>
2024-07-19 15:00:11 +0700

    commit

--- pzn/StandardLibrary/Day.js
+++ pzn/StandardLibrary/Day.js
@@ -1,28 +1,16 @@
 
+function intervalClock() {
+    setInterval(() => {
+        const date = new Date();
 
-const date= new Date();
+        const hour = date.getHours().toString().padStart(2, '0');
+        const minutes = date.getMinutes().toString().padStart(2, '0');
+        const seconds = date.getSeconds().toString().padStart(2, '0');
+        const day = date.toDateString();
 
-let hour = date.getHours();
-let minutes = date.getMinutes();
-let seconds = date.getSeconds();
-const day = date.toDateString()
-
-function intervalClock(){
-    setInterval(()=>{
-        
-        const logDate = `Hari: ${day}, jam: ${hour}:${minutes}:${seconds}`;
+        const logDate = `Hari: ${day}, waktu menunjukkan: ${hour}:${minutes}:${seconds}`;
         console.log(logDate);
-    }, 1000)
+    }, 1000);
 }
 
-function parsingDate(){
-    const timeNow = date.parse(`2020-10-10T08:45:45.123+07:00`);
-    console.log(timeNow);
-
-    const theDate = date(timeNow)
-    console.log(theDate);
-
-}
-
-(()=>{intervalClock(); parsingDate();})();
-
+(() => intervalClock())();
