e16ecd0d41559d2fd76abffc54a15acfa5736608
teukureynld <teukureynaldi106@gmail.com>
2024-09-01 01:13:45 +0700

    updated

--- golang/pointers/pointers.go
+++ golang/pointers/pointers.go
@@ -1,10 +1,20 @@
-package pointers;
+package main;
 
 import (
-	"fmt",
+	"fmt"
 )
 
-func helloWorld(){
-	// p untuk pointers, 
-	fmt.Println("HelloWorld")
+func main(){
+	var age int = 23;
+	var valueOfPointer = &age;
+	fmt.Println(valueOfPointer)
+
+}
+
+func setPointersString(name *string, newAddressOfName string){
+	return
+}
+
+func getPointers(){
+	return;
 }
